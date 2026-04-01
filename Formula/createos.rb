0bb05f5184684c55aa9c9751bbc6a442bcd057db
Bhautik <bhautikrchudasama@gmail.com>
2026-04-01 14:18:14 +0530

    Add Linux support to formula

--- Formula/createos.rb
+++ Formula/createos.rb
@@ -16,9 +16,22 @@ class Createos < Formula
     end
   end
 
+  on_linux do
+    on_arm do
+      url "https://github.com/NodeOps-app/createos-cli/releases/download/v0.0.3/createos-linux-arm64"
+      sha256 "4f8a2eac858a6ac498e1593f1b71105416f8bf2eb68364f04e3968a43ef48cdb"
+    end
+
+    on_intel do
+      url "https://github.com/NodeOps-app/createos-cli/releases/download/v0.0.3/createos-linux-amd64"
+      sha256 "ff603f7c5200544086c9f1dd3d2b8213916a0693a018dbed0d395599986ec464"
+    end
+  end
+
   def install
-    binary = Hardware::CPU.arm? ? "createos-darwin-arm64" : "createos-darwin-amd64"
-    bin.install binary => "createos"
+    os = OS.mac? ? "darwin" : "linux"
+    arch = Hardware::CPU.arm? ? "arm64" : "amd64"
+    bin.install "createos-#{os}-#{arch}" => "createos"
   end
 
   test do
